ac2104c7b5de5a0586863ca78f6a3b7c4f500a93
Brian Sheridan <f7digitaldesign@gmail.com>
2025-07-24 06:44:42 -0700

    Update layout.tsx
    Added Google GA4 Analytics for BootPrices home page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MXBVYBXDQY"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MXBVYBXDQY');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
