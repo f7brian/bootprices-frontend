3f4e1b8c81f9232fec2a3e1a6b63e5321f25165d
Brian Sheridan <f7digitaldesign@gmail.com>
2025-08-11 07:00:28 -0700

    Update ProductGrid.tsx
    Make product grid on Mobile 2 columns

--- src/components/Home/Product/ProductGrid.tsx
+++ src/components/Home/Product/ProductGrid.tsx
@@ -823,7 +823,7 @@ export default function ProductGrid() {
                 </div>
               ) : (
                 <>
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {products?.products?.map((product: Product) => (
                       <Card
                         key={product.ASIN}
